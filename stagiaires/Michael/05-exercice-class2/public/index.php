680c102559ed5f3897e61976790b7aa50d4b41b6
WebDevCF2m2026 <gitweb@cf2m.be>
2026-05-11 12:08:05 +0200

    create path to first view

--- stagiaires/Michael/05-exercice-class2/public/index.php
+++ stagiaires/Michael/05-exercice-class2/public/index.php
@@ -2,6 +2,7 @@
 // 05-exercice/public/index.php
 
 # Importation de ../config-dev.php 
-
+require_once "../config-dev.php";
 
 # Importation du router : ROOT_PROJECT."/controller/routerController.php"
+require_once ROOT_PROJECT."/controller/routerController.php";
