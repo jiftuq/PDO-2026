28eda16254d5aa0abb5acddf5074437a38d570fa
LaetitiaBraun <laetitia.pianiste@gmail.com>
2026-05-19 09:58:37 +0200

    Mises à jour continue

--- stagiaires/Laetitia/06-exercice-classe1/config-dev.php
+++ stagiaires/Laetitia/06-exercice-classe1/config-dev.php
@@ -3,10 +3,10 @@
 
 // variable de développement, à dupliquer
 // son le nom de config.php
-const DB_CONNECT_TYPE = "mysql"; // MySQL et MariaDB
+const DB_CONNECT_TYPE = "mysql";
 const DB_CONNECT_HOST = "localhost";
-const DB_CONNECT_PORT = 3307;// WAMP -> MariaDB
-const DB_CONNECT_NAME = "";
+const DB_CONNECT_PORT = 3306;
+const DB_CONNECT_NAME = "exe_06";
 const DB_CONNECT_CHARSET = "utf8mb4";
 const DB_CONNECT_USER = "root";
 const DB_CONNECT_PWD = "";
--- stagiaires/Laetitia/06-exercice-classe1/public/index.php
+++ stagiaires/Laetitia/06-exercice-classe1/public/index.php
@@ -1,8 +1,10 @@
 <?php
 // stagiaires/Michael/06-exercice-classe1/public/index.php
 
+session_start();
+
 # Importation de ../config-dev.php
 require_once "../config-dev.php";
 
-# Importation du router : ROOT_PROJECT."/controller/routerController.php"
+# Importation du router
 require ROOT_PROJECT."/controller/routerController.php";
