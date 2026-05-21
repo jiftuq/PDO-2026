9ca9eeb63ac375690668b24bbff644964a4d8572
WebDevCF2m <gitweb@cf2m.be>
2026-05-21 13:55:00 +0200

    MYsl to mariadb

--- stagiaires/Laetitia/06-exercice-classe1/config-dev.php
+++ stagiaires/Laetitia/06-exercice-classe1/config-dev.php
@@ -5,7 +5,7 @@
 // son le nom de config.php
 const DB_CONNECT_TYPE = "mysql";
 const DB_CONNECT_HOST = "localhost";
-const DB_CONNECT_PORT = 3306;
+const DB_CONNECT_PORT = 3307;
 const DB_CONNECT_NAME = "exe_06";
 const DB_CONNECT_CHARSET = "utf8mb4";
 const DB_CONNECT_USER = "root";
