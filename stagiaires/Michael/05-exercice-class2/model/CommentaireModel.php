9f7593b284bf4250631313f437702c4c2ad65e20
WebDevCF2m2026 <gitweb@cf2m.be>
2026-05-22 09:51:33 +0200

    function readCommentaires(PDO ): array

--- stagiaires/Michael/05-exercice-class2/model/CommentaireModel.php
+++ stagiaires/Michael/05-exercice-class2/model/CommentaireModel.php
@@ -6,8 +6,17 @@ function addCommentaire(){
 
 }
 
-function readCommentaires(){
-
+// chargement de tous les commentaires
+function readCommentaires(PDO $db): array
+{
+    // requête
+    $stmt = $db->query("SELECT * FROM `commentaire` ORDER BY `post_date` DESC");
+    // recupération des resultats en fetch_assoc (voir connexion)
+    $result = $stmt->fetchAll();
+    // bonne pratique
+    $stmt->closeCursor();
+    // retour
+    return $result;
 }
 
 // bonus pagination
