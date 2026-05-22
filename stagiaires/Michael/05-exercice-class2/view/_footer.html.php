23858a5376815ec1a4aabb27fc6ceea0afec16bf
WebDevCF2m2026 <gitweb@cf2m.be>
2026-05-22 12:03:11 +0200

    no time, no name

--- stagiaires/Michael/05-exercice-class2/view/_footer.html.php
+++ stagiaires/Michael/05-exercice-class2/view/_footer.html.php
@@ -4,7 +4,7 @@
     <p>&copy; <?= date('Y') ?> Mon Site</p>
 </footer>
 
-<!--<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="js/script.js"></script>-->
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="js/script.js"></script>
 </body>
 </html>
